c2bc098b996a339931b3cbd83a85838acb59c84f
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-03-13 18:41:07 -0300

    working on design

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,18 @@
 import './style.css';
+import dogua from '../../assets/dogua.png';
 
 export default function Card() {
     return(
         <div className="card">
+            <img src={dogua} alt="adImage"/>
             <div className="content">
-                <p>Nome</p>
-                <p>Descriçao</p>
-                <p>contato</p>
-                <p>endereco</p>
+                
+                <p>    No entanto, não podemos esquecer que o 
+                    acompanhamento das preferências de consumo afeta positivamente a correta previsão 
+                    das condições inegavelmente apropriadas.
+                </p>
+                <p>21 9999999999 - Fulano de Tal</p>
+                <p>Rua ze das couves 30 - barra mansa, RJ</p>
             </div>
         </div>
     );
